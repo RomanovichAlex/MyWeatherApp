919e89c2a6a9f718b724c6b97cdc8478b03ed9a0
RomanovichAlex <romanowitch.alex@yandex.ru>
2022-01-20 13:48:52 +0300

    Урок 6. Накосячил с гит

--- app/src/main/java/by/romanovich/theweatherapp/lesson6/ThreadsFragment.kt
+++ app/src/main/java/by/romanovich/theweatherapp/lesson6/ThreadsFragment.kt
@@ -6,6 +6,7 @@ import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import by.romanovich.theweatherapp.databinding.FragmentThreadsBinding
 
@@ -18,22 +19,42 @@ class ThreadsFragment : Fragment() {
             return _binding!!
         }
 
+
+    //создали один поток для этого фрагмента и выполнения задач
+    val myThread = MyThread()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        myThread.start()
+        //по клику на кнопку передаем потоку наши задачи
         binding.button.setOnClickListener {
-            Thread{
-                //в вспомогательном потоке
-                val result = startCalculations(2) // крупная задача
-                //в главном потоке можно двумя способомами
-                requireActivity().runOnUiThread {
-                    //binding.textView.text=result
+            myThread.handler?.post {
+                //обработка задач
+                val result = startCalculations(2)
+                activity?.let{ activity->
+                    //
+                    Handler(Looper.getMainLooper()).post {
+                        //создаем новые вьюхи на каждый из наших потоков, добавляя их в mainContainer
+                        binding.mainContainer.addView(TextView(activity).apply {
+                            text = result
+                        })
+                    }
                 }
-                Handler(Looper.getMainLooper()).post {
-                    binding.textView.text=result
-                }
-            }.start()
+            }
         }
     }
+
+
+    //что бы не создавать кучу потоков, будем работать в одном
+    class MyThread:Thread(){
+        var handler: Handler?= null
+        override fun run() {
+            //зацикливаем, поток(бесконечные поток, чтобы не закрывался по сле выполнения задачи)
+            Looper.prepare()
+            handler = Handler(Looper.myLooper()!!)
+            Looper.loop()
+        }
+    }
+
 
     private fun startCalculations(seconds: Int): String {
         Thread.sleep(seconds*1000L)
